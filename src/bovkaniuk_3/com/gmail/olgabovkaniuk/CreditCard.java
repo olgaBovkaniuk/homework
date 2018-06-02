f9fb67e6ba566a5e4bb5df60ef10e2c9b83cb17c
olgaBovkaniuk <olgabovkaniuk@gmail.com>
2018-06-03 00:12:33 +0300

    Bovkaniuk_3

---
+++ src/bovkaniuk_3/com/gmail/olgabovkaniuk/CreditCard.java
@@ -0,0 +1,32 @@
+package bovkaniuk_3.com.gmail.olgabovkaniuk;
+
+public class CreditCard {
+    private int accountNumber;
+    private double currentAmount;
+
+    public CreditCard(int accountNumber, double currentAmount) {
+        this.accountNumber = accountNumber;
+        this.currentAmount = currentAmount;
+    }
+
+    public double getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getCurrentAmount() {
+        return currentAmount;
+    }
+
+    public void charge(double chargeSum) {
+        currentAmount += chargeSum;
+    }
+
+    public void writeOff(double writeOffSum) {
+        currentAmount -= writeOffSum;
+    }
+
+    public void currentAccountInfo() {
+        System.out.println("Account number is " + accountNumber);
+        System.out.println("Current amount is " + currentAmount);
+    }
+}
---
+++ src/bovkaniuk_3/com/gmail/olgabovkaniuk/CreditCardApp.java
@@ -0,0 +1,17 @@
+package bovkaniuk_3.com.gmail.olgabovkaniuk;
+
+public class CreditCardApp {
+    public static void main(String[] args) {
+        CreditCard creditCard1 = new CreditCard(1, 95.5);
+        CreditCard creditCard2 = new CreditCard(2, 100.0);
+        CreditCard creditCard3 = new CreditCard(3, 73.7);
+
+        creditCard1.charge(15.5);
+        creditCard2.charge(25.6);
+        creditCard3.writeOff(13.7);
+
+        creditCard1.currentAccountInfo();
+        creditCard2.currentAccountInfo();
+        creditCard3.currentAccountInfo();
+    }
+}
